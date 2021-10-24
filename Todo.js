4055b31efc05a90d9aa114946f2b62da42ca53ae
mksahil <86847852+mksahil@users.noreply.github.com>
2021-10-24 18:10:24 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,51 @@
+add = document.getElementById("add");
+add.addEventListener("click", GetAndUpdate);
+update();
+function GetAndUpdate() {
+  til = document.getElementById("titel").value;
+  desc = document.getElementById("description").value;
+  if (localStorage.getItem("itemsJson") == null) {
+    itemJsonArray = [];
+    itemJsonArray.push([til, desc]);
+    localStorage.setItem("itemsJson", JSON.stringify(itemJsonarray));
+  } else {
+    itemJsonArraystr = localStorage.getItem("itemsJson");
+    itemJsonArray = JSON.parse(itemJsonArraystr);
+    itemJsonArray.push([til, desc]);
+    localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  }
+  update();
+}
+function update() {
+  if (localStorage.getItem("itemsJson") == null) {
+    itemJsonArray = [];
+    localStorage.setItem("itemsJson", JSON.stringify(itemJsonarray));
+  } else {
+    itemJsonArraystr = localStorage.getItem("itemsJson");
+    itemJsonArray = JSON.parse(itemJsonArraystr);
+  }
+
+  //   populate the table
+  let tabelbody = document.getElementById("tabelbody");
+  let str = "";
+  itemJsonArray.forEach((element, index) => {
+    str += `
+                    <tr>
+                        <th scope="row">${index + 1}</th>
+                        <td>${element[0]}</td>
+                        <td>${element[1]}</td>
+                        <td> <button class="btn btn-sm btn-primary" onclick="deleted(${index})">Delete</button></td>
+                     </tr> `;
+  });
+  tabelbody.innerHTML = str;
+}
+
+function deleted(itemindex) {
+  console.log("Delete", itemindex);
+  itemJsonArraystr = localStorage.getItem("itemsJson");
+  itemJsonArray = JSON.parse(itemJsonArraystr);
+
+  itemJsonArray.splice(itemindex, 1);
+  localStorage.setItem("itemsJson", JSON.stringify(itemJsonArray));
+  update();
+}
